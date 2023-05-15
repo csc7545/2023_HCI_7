717c194c57b32e3253ba6316f2b57399a54f8a71
Yeggggi <49176310+Yeggggi@users.noreply.github.com>
2023-05-16 00:49:40 +0900

    [UPDATE] camera available

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        builder: () => MaterialApp(
+        builder: (context, child) => MaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'Nap-Guardian',
               theme: ThemeData(
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/choeseongchan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/choeseongchan/StudioProjects/tensorFlow-Flutter"
+export "FLUTTER_ROOT=/Users/yejiseo/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/yejiseo/Documents/hci_7/2023_HCI_7"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=1.1.1"
+export "FLUTTER_BUILD_NUMBER=1.1.1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
