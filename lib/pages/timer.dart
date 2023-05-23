050f1250e0608145f4362f7aca90efb4fd5adc83
hosoong <107905789+hosoong@users.noreply.github.com>
2023-05-23 20:42:25 +0900

    [Update]Timer

--- lib/pages/timer.dart
+++ lib/pages/timer.dart
@@ -1,9 +1,17 @@
+
+
+import 'package:clean_dialog/clean_dialog.dart';
+import 'package:emerge_alert_dialog/emerge_alert_dialog.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
 import 'package:animated_snack_bar/animated_snack_bar.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:hp/pages/camera.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:hp/utils/mlkit_utils.dart';
@@ -11,6 +19,16 @@ import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:camerawesome/pigeon.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 import 'package:rxdart/rxdart.dart';
+
+class ReactiveController extends GetxController {
+  RxInt counter = 0.obs;
+  RxInt _timercnt = 0.obs;
+  void increase(int time) {
+    if(time % 10 == 0){
+      counter++;
+    }
+  }
+}
 
 class MyTimer extends StatefulWidget {
   final String breakTime;
@@ -30,8 +48,10 @@ class MyTimer extends StatefulWidget {
 
 class _TimerState extends State<MyTimer> {
   bool _isRunning = false;
+  bool _ischeck = false;
   Duration _time = const Duration(minutes: 60);
   Duration _break = const Duration(minutes: 10);
+  Duration _checktime = const Duration(minutes: 10);
   int _timeInt = 60;
   int _counter = 1;
   int _sessionCount = 4;
@@ -39,6 +59,9 @@ class _TimerState extends State<MyTimer> {
   int _currMax = 60;
   Timer? _timer;
   SharedPreferences? _prefs;
+  int _seconds = 0;
+  bool _checktimer = false;
+  late Timer _timecheck;
 
   @override
   void initState() {
@@ -86,9 +109,8 @@ class _TimerState extends State<MyTimer> {
                 ),
                 Row(
                   children: const [
-                    SizedBox(
-                        width:
-                            50), // Add some horizontal spacing to align the text with the first message
+                    SizedBox(width: 50),
+                    // Add some horizontal spacing to align the text with the first message
                     Text(
                       "Please enter valid numbers to start.",
                       style: TextStyle(
@@ -177,9 +199,8 @@ class _TimerState extends State<MyTimer> {
                       ),
                       Row(
                         children: [
-                          const SizedBox(
-                              width:
-                                  50), // Add some horizontal spacing to align the text with the first message
+                          const SizedBox(width: 50),
+                          // Add some horizontal spacing to align the text with the first message
                           Text(
                             'You logged ${_sessionCount * _timeInt} minutes.',
                             style: TextStyle(
@@ -198,6 +219,32 @@ class _TimerState extends State<MyTimer> {
             FocusManager.instance.primaryFocus?.unfocus();
             _storeTime();
             Navigator.pop(context);
+          } else if (_sessionCount == _counter / 2) {
+            showDialog(
+              context: context,
+              builder: (context) => CleanDialog(
+                title: 'Error',
+                content: 'We were not able to update your information.',
+                backgroundColor: const Color(0XFFbe3a2c),
+                titleTextStyle: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                contentTextStyle:
+                    const TextStyle(fontSize: 16, color: Colors.white),
+                actions: [
+                  CleanDialogActionButtons(
+                    actionTitle: 'Cancel',
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  CleanDialogActionButtons(
+                    actionTitle: 'Try again',
+                    textColor: const Color(0XFF27ae61),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            );
           }
 
           _stopTimer();
@@ -226,8 +273,18 @@ class _TimerState extends State<MyTimer> {
     });
   }
 
+  void _startcheckTimer() {
+    _checktimer = true;
+    _timecheck = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _seconds++;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Get.put(ReactiveController());
     final int minutes = _time.inMinutes;
     final int seconds = _time.inSeconds % 60;
     String timerState = "Break";
@@ -235,109 +292,227 @@ class _TimerState extends State<MyTimer> {
       timerState = '$_counter / $_sessionCount';
     }
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: false,
         backgroundColor: Colors.black,
-        title: Text.rich(
-          TextSpan(
-            text: 'Study', // text for title
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.orangeAccent,
-              fontFamily: 'Arial',
-            ),
-          ),
-        ),
-
-        // Create a button to pause/resume the timer
-        actions: [
-          IconButton(
-            padding: const EdgeInsets.only(right: 20.0),
-            icon: const Icon(Icons.restart_alt,
-                color: Colors.orangeAccent, size: 30),
-            onPressed: () {
-              setState(() {
-                _resetTimer();
-              });
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: CameraPage(),
-            ),
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Stack(
-                children: [
-                  Container(
-                    width: 300,
-                    height: 300,
-                    child: CircularProgressIndicator(
-                      color: Colors.orangeAccent,
-                      backgroundColor: Colors.black,
-                      value: _time.inSeconds /
-                          (_currMax *
-                              60), // calculates the progress as a value between 0 and 1
-                      strokeWidth: 2,
-                    ),
-                  ),
-                  Positioned(
-                    top: 100,
-                    left: 70,
-                    child: Text(
-                      '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}',
-                      style: const TextStyle(
-                        fontSize: 60,
-                        color: Colors.orangeAccent,
-                        fontFamily: 'Arial',
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 100,
-                    left: 130,
-                    child: Text(
-                      timerState,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.orangeAccent,
-                        fontFamily: 'Arial',
-                      ),
-                    ),
-                  ),
-                ],
+        appBar: AppBar(
+          centerTitle: false,
+          backgroundColor: Colors.black,
+          title: Text.rich(
+            TextSpan(
+              text: 'Study', // text for title
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.orangeAccent,
+                fontFamily: 'Arial',
               ),
+            ),
+          ),
+
+          // Create a button to pause/resume the timer
+          actions: [
+            IconButton(
+              padding: const EdgeInsets.only(right: 20.0),
+              icon: const Icon(Icons.restart_alt,
+                  color: Colors.orangeAccent, size: 30),
+              onPressed: () {
+                setState(() {
+                  _resetTimer();
+                });
+              },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if (_isRunning) {
-              _stopTimer();
-            } else {
-              _startTimer();
-            }
-            _isRunning = !_isRunning;
-          });
-        },
-        shape: const CircleBorder(),
-        backgroundColor: Colors.black,
-        mini: false,
-        child: _isRunning
-            ? const Icon(Icons.pause, color: Colors.orangeAccent)
-            : const Icon(Icons.play_arrow, color: Colors.orangeAccent),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: CameraPage(),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 300,
+                      height: 300,
+                      child: CircularProgressIndicator(
+                        color: Colors.orangeAccent,
+                        backgroundColor: Colors.black,
+                        value: _time.inSeconds / (_currMax * 60),
+                        // calculates the progress as a value between 0 and 1
+                        strokeWidth: 2,
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                      left: 70,
+                      child: Text(
+                        '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}',
+                        style: const TextStyle(
+                          fontSize: 60,
+                          color: Colors.orangeAccent,
+                          fontFamily: 'Arial',
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 100,
+                      left: 130,
+                      child: Text(
+                        timerState,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.orangeAccent,
+                          fontFamily: 'Arial',
+                        ),
+                      ),
+                    ),
+                    // Positioned(
+                    //   bottom: 100,
+                    //   left: 130,
+                    //   child: GetX<ReactiveController>( // 반응형 상태관리 - 1
+                    //   builder: (controller) {
+                    //     return ElevatedButton(
+                    //       child: Text(
+                    //         '반응형 1 / 현재 숫자: ${controller.counter.value}', // .value 로 접근
+                    //       ),
+                    //       onPressed: () {
+                    //         controller.increase(_seconds);
+                    //         print('$_seconds');
+                    //         // Get.find<ReactiveController>().increase();
+                    //       },
+                    //     );
+                    //   },
+                    // ),),
+                    Positioned(
+                      bottom: 150,
+                      left: 130,
+                      child: Row(
+                        children: [
+                          TextButton(onPressed: (){
+                            //print(_ischeck);
+                            _startcheckTimer();
+                            setState(() {
+                              _ischeck = !_ischeck;
+                              _ischeck? print(_seconds) : print('false');
+                            });
+                          }, child: Text('+10')),
+                          TextButton(onPressed: (){
+                            if(_seconds % 10 == 0){
+                              print('10dmlqotn');
+                              //++_timerCount;
+                            }
+                            else{
+                              print('10xxxxxxx');
+                            }
+                            print('$_timerCount');
+                            print('$_seconds');
+                            },
+                              child: Text('check'))
+                        ],
+                      )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          children: [
+            TextButton(
+              onPressed: () => _showMyDialog(context),
+              child: const Text("Show PopUp"),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+
+                  if (_isRunning) {
+                    _stopTimer();
+                  } else {
+                    _startTimer();
+                  }
+                  _isRunning = !_isRunning;
+                }
+                //if
+                );
+              },
+              shape: const CircleBorder(),
+              backgroundColor: Colors.black,
+              mini: false,
+              child: _isRunning
+                  ? const Icon(Icons.pause, color: Colors.orangeAccent)
+                  : const Icon(Icons.play_arrow, color: Colors.orangeAccent),
+            ),
+          ],
+        ));
+  }
+  Future<void> _showMyDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return EmergeAlertDialog(
+          alignment: Alignment.bottomRight,
+          emergeAlertDialogOptions: EmergeAlertDialogOptions(
+            title: const Text("Privacy Info"),
+            content: _content(),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _content() {
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/stretch.jpg"),
+          const SizedBox(height: 22.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Container(
+                  height: size.height * 0.045,
+                  width: size.width * 0.3,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: const Text(
+                    "Cancel",
+                    style: TextStyle(color: Colors.black45),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                height: size.height * 0.045,
+                width: size.width * 0.3,
+                alignment: Alignment.center,
+                child: const Text(
+                  "Confirm",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+
 }
+
